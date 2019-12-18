b49d9405e3f2391364706952033c1a168c2a4ec2
JustSenoko <just.senoko@gmail.com>
2019-12-18 09:31:41 +0300

    bottom navigation

--- app/src/main/java/com/blueroofstudio/materialdesignapp/ui/activities/BottomNavigationActivity.java
+++ app/src/main/java/com/blueroofstudio/materialdesignapp/ui/activities/BottomNavigationActivity.java
@@ -1,10 +1,16 @@
 package com.blueroofstudio.materialdesignapp.ui.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 import com.blueroofstudio.materialdesignapp.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 public class BottomNavigationActivity extends AppCompatActivity {
 
@@ -12,5 +18,16 @@ public class BottomNavigationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_navigation);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_progress_bar, R.id.nav_map, R.id.nav_tab3)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
     }
+
 }
